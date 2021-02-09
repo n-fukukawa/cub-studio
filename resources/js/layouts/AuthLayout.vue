347e4e476adc47b7d326587773a4a6c9f01a41b3
Naruki Fukukawa <alnair9501@gmail.com>
2021-02-09 11:09:49 +0900

    レイアウトの修正

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,8 +1,8 @@
 <template>
 <div>
-    <div class="h-screen flex items-center -mt-12">
-        <div class="w-4/5 mx-auto px-4 py-8 bg-white shadow rounded sm:grid sm:grid-cols-2 sm:max-w-3xl sm:py-16 sm:px-8">
-            <div class="flex items-center justify-center font-logo text-3xl font-bold text-gray-700 tracking-superwide my-12 sm:my-0">
+    <div class="mt-4">
+        <div class="w-full mx-auto px-4 py-8 bg-white shadow rounded sm:grid sm:grid-cols-2 sm:max-w-3xl sm:py-16 sm:px-8">
+            <div class="flex items-center justify-center font-logo text-3xl font-bold text-gray-700 tracking-superwide mt-4 mb-8 sm:my-0">
                 <slot name="heading" />
             </div>
             <div class="flex items-center justify-center">
