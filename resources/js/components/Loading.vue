900bd64ca6c28cd43eac79d6c06bf789a52ad122
Naruki Fukukawa <alnair9501@gmail.com>
2021-02-09 11:12:43 +0900

    Update: レイアウト変更

--- resources/js/components/Loading.vue
+++ resources/js/components/Loading.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="fixed z-40 top-0 left-0 flex justify-items-center place-items-center text-center h-screen w-full bg-white">
-        <div class="flex">
+        <div class="flex mx-auto">
             <img src="/img/loadings.gif">
-            <p class="font-logo ml-4">Now Loading...</p>
+            <p class="font-logo text-xl ml-4">Now Loading...</p>
         </div>
     </div>
 </template>
